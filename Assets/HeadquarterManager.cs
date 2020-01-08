3522a0ea0ae42a57f4e80a75287d2201d65d50e7
lionel_wood <lionel@metia.co.nz>
2020-01-08 20:37:17 +1300

    - starting hq mount point logic.

---
+++ Assets/HeadquarterManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadquarterManager : MonoBehaviour
+{
+    [SerializeField] string _mountPointID;
+    [SerializeField] Headquarters _headquartersPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var mp in WorldMapStrategyKit.WMSK.instance.mountPoints)
+        {
+            var go = Instantiate(_headquartersPrefab);
+            go.transform.position = mp.unity2DLocation;
+            go.name = mp.name;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
