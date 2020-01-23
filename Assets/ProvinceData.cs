0c0403bbd8aeb114c501142de96fb3f2a0caa5c7
lionel_wood <lionel@metia.co.nz>
2020-01-24 10:55:20 +1300

    - province local support variable testing

---
+++ Assets/ProvinceData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using WorldMapStrategyKit;
+
+internal class ProvinceData : MonoBehaviour
+{
+    Province _prov;
+
+    [Range(0f, 1f)]
+    [SerializeField] float _localSupportFraction;
+
+    internal void Initialise(Province prov)
+    {
+        _prov = prov;
+
+        SetDebugSupport();
+    }
+
+    private void SetDebugSupport()
+    {
+        _localSupportFraction = UnityEngine.Random.Range(0f, 1f);
+    }
+}
---
+++ Assets/Province_Manager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WorldMapStrategyKit;
+
+public class Province_Manager : MonoBehaviour
+{
+    public static Province_Manager Instance => _instance;
+    static Province_Manager _instance;
+
+    [SerializeField] List<ProvinceData> _provinceList;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _instance = this;
+
+        BuildProvinceList();
+    }
+
+    private void BuildProvinceList()
+    {
+        // TODO Make these non-gameobjects, have a custom window to inspect them
+        _provinceList.Clear();
+        foreach (var prov in WMSK.instance.provinces)
+        {
+            GameObject go = new GameObject($"{prov.name}");
+            go.transform.SetParent(transform);
+            ProvinceData provData = go.AddComponent<ProvinceData>();
+            provData.Initialise(prov);
+            _provinceList.Add(provData);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
