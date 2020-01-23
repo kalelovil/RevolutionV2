d3ef7be66da878122d558ef275d88347beeb3385
lionel_wood <lionel@metia.co.nz>
2020-01-24 12:43:06 +1300

    - added manpower field to provincedata

--- Assets/ProvinceData.cs
+++ Assets/ProvinceData.cs
@@ -9,6 +9,11 @@ internal class ProvinceData : MonoBehaviour
     [Range(0f, 1f)]
     [SerializeField] float _localSupportFraction;
 
+    const int Max_Manpower = 20;
+    [Range(0, Max_Manpower)]
+    [SerializeField] int _manpower;
+    public int Manpower { get => _manpower; set => _manpower = value; }
+
     internal void Initialise(Province prov)
     {
         _prov = prov;
@@ -19,5 +24,7 @@ internal class ProvinceData : MonoBehaviour
     private void SetDebugSupport()
     {
         _localSupportFraction = UnityEngine.Random.Range(0f, 1f);
+
+        Manpower = (int)(Max_Manpower * _localSupportFraction);
     }
 }
