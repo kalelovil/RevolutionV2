3f0cc1e7a00784fd0c4cf2e3a7ffecff22c5779c
lionel_wood <lionel@metia.co.nz>
2020-01-25 10:48:24 +1300

    - turn on/off province panel visuals including province name panel.

--- Assets/UI_ProvincePanel.cs
+++ Assets/UI_ProvincePanel.cs
@@ -2,16 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using WorldMapStrategyKit;
 
 public class UI_ProvincePanel : MonoBehaviour
 {
-    static UI_ProvincePanel _instance;
-    public static UI_ProvincePanel Instance => _instance;
+    [Header("Data")]
+    [SerializeField] ProvinceData _provinceData;
+    public ProvinceData ProvinceData { get { return _provinceData; } internal set { ProvinceSelected(value); } }
+    private void ProvinceSelected(ProvinceData value)
+    {
+        _provinceData = value;
+        _provinceNameText.text = ProvinceData.Province.name;
+    }
 
-    public ProvinceData ProvinceData { get; internal set; }
+    [Header("UI")]
+    [SerializeField] GameObject _visualsParentObject;
+    [SerializeField] TextMeshProUGUI _provinceNameText;
 
     [SerializeField] private UI_Recruitment_Panel _recruitmentPanel;
+
+    static UI_ProvincePanel _instance;
+    public static UI_ProvincePanel Instance => _instance;
 
     // Start is called before the first frame update
     void Awake()
@@ -27,7 +39,7 @@ public class UI_ProvincePanel : MonoBehaviour
 
     internal void OpenRecruitmentPanel()
     {
-        _recruitmentPanel.gameObject.SetActive(true);
+        _visualsParentObject.SetActive(true);
     }
 
     internal void RecruitUnit(Unit unitType)
