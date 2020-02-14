76c30ded82864c4de9d50564af039b77b2e799d6
lionel_wood <lionel@metia.co.nz>
2020-02-14 14:12:12 +1300

    - ui elements part 1

--- Assets/UI_AbstractAgent_Icon.cs
+++ Assets/UI_AbstractAgent_Icon.cs
@@ -7,9 +7,11 @@ using TMPro;
 public abstract class UI_AbstractAgent_Icon : MonoBehaviour
 {
     [SerializeField] Image _imageIcon;
+
     [SerializeField] TextMeshProUGUI _nameText;
     [SerializeField] TextMeshProUGUI _levelText;
     [SerializeField] TextMeshProUGUI _speedText;
+
     [SerializeField] Transform _elementArea;
 
     // Start is called before the first frame update
---
+++ Assets/UI_BrigadeLeader_Icon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_BrigadeLeader_Icon : UI_AbstractAgent_Icon
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
